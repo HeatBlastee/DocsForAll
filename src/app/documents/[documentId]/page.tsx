2dec19551b0994ec87ec49fa16f59036c905f072
HeatBlastee <mann42006@gmail.com>
2025-03-29 18:43:26 +0530

    Basic Page added and toolbar build starting

---
+++ src/app/documents/[documentId]/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Editor } from './editorPage';
+import Toolbar from './toolbar';
+const DocumentIdPage = async () => {
+  return (
+    <div className='min-h-screen bg-[#FAFBFD]'>
+
+
+      <Toolbar />
+
+      <Editor />
+
+    </div>
+  )
+}
+
+export default DocumentIdPage

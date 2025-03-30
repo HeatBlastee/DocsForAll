51bb8e3614a90556de642db937fd61424d0a3f13
HeatBlastee <mann42006@gmail.com>
2025-03-30 16:27:07 +0530

    Basic Toolbar added

--- src/app/documents/[documentId]/editorPage.tsx
+++ src/app/documents/[documentId]/editorPage.tsx
@@ -1,5 +1,4 @@
 'use client'
-
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import TaskItem from '@tiptap/extension-task-item'
@@ -8,34 +7,60 @@ import Table from '@tiptap/extension-table'
 import TableCell from '@tiptap/extension-table-cell'
 import TableHeader from '@tiptap/extension-table-header'
 import TableRow from '@tiptap/extension-table-row'
+import Underline from '@tiptap/extension-underline'
 import Image from '@tiptap/extension-image'
-
+import { useEditorStore } from '@/store/use-editor-store'
 import ImageResize from 'tiptap-extension-resize-image';
 export const Editor = () => {
-    const editor = useEditor({
-        editorProps: {
-            attributes: {
-                style: "padding-left: 56px; padding-right: 56px;",
-                class: "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
-            }
-        },
+  const { setEditor } = useEditorStore();
+  const editor = useEditor({
+    onCreate({ editor }) {
+      setEditor(editor);
+    },
+    onDestroy() {
+      setEditor(null);
+    },
+    onUpdate({ editor }) {
+      setEditor(editor);
+    },
+    onSelectionUpdate({ editor }) {
+      setEditor(editor);
+    },
+    onTransaction({ editor }) {
+      setEditor(editor);
+    },
+    onFocus({ editor }) {
+      setEditor(editor);
+    },
+    onBlur({ editor }) {
+      setEditor(editor);
+    },
+    onContentError({ editor }) {
+      setEditor(editor);
+    },
+    editorProps: {
+      attributes: {
+        style: "padding-left: 56px; padding-right: 56px;",
+        class: "focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
+      }
+    },
+    extensions: [StarterKit,
+      TaskList,
+      TaskItem.configure({
+        nested: true,
+      }),
+      Table.configure({
+        resizable: true,
+      }),
+      TableRow,
+      TableHeader,
+      TableCell,
+      Image,
+      ImageResize,
+      Underline
 
-        extensions: [StarterKit,
-            TaskList,
-            TaskItem.configure({
-                nested: true,
-            }),
-            Table.configure({
-                resizable: true,
-            }),
-            TableRow,
-            TableHeader,
-            TableCell,
-            Image,
-            ImageResize
-
-        ],
-        content: `
+    ],
+    content: `
         <table>
           <tbody>
             <tr>
@@ -51,15 +76,14 @@ export const Editor = () => {
           </tbody>
         </table>
       `,
-    })
+  });
+  return (
+    <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
 
-    return (
-        <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
+      <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
+        <EditorContent editor={editor} />
 
-            <div className="min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0">
-                <EditorContent editor={editor} />
-
-            </div>
-        </div>
-    )
+      </div>
+    </div>
+  )
 }
--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -1,8 +1,10 @@
 "use client"
+import { Separator } from '@/components/ui/separator';
 import { cn } from '@/lib/utils';
-import { LucideIcon, Undo2Icon } from 'lucide-react';
+import { useEditorStore } from '@/store/use-editor-store';
+import { BoldIcon, ItalicIcon, ListTodoIcon, LucideIcon, MessageSquarePlusIcon, PrinterIcon, Redo2Icon, RemoveFormattingIcon, SpellCheckIcon, UnderlineIcon, Undo2Icon } from 'lucide-react';
 import React from 'react'
-interface ToolbarButtonProps{
+interface ToolbarButtonProps {
     onClick?: () => void;
     isActive?: boolean;
     icon: LucideIcon;
@@ -12,8 +14,8 @@ interface ToolbarButtonProps{
 const ToolbarButton = ({
     onClick,
     isActive,
-    icon:Icon,
-}: ToolbarButtonProps)=>{
+    icon: Icon,
+}: ToolbarButtonProps) => {
     return (
         <button
             onClick={onClick}
@@ -21,11 +23,14 @@ const ToolbarButton = ({
                 "text-sm h-7 min-w-7 flex items-center justify-center rounded-sm hover:bg-neutral-200/80",
                 isActive && "bg-neutral-200/80"
             )}
-        ><Icon className='size-4'/></button>
+        ><Icon className='size-4' /></button>
 
     )
 }
 const Toolbar = () => {
+    const { editor } = useEditorStore();
+
+    console.log("Toolbar Editor : ", editor)
     const sections: {
         label: string;
         icon: LucideIcon;
@@ -33,20 +38,88 @@ const Toolbar = () => {
         isActive?: boolean;
     }[][] = [
             [
-            {
+                {
                     label: "Undo",
                     icon: Undo2Icon,
-                onClick:()=>console.log("Undo Clicked")
-            },
-        ],
-    ]
-  return (
-    <div className='bg-[#F1F4F9] px-2.5 py-0.5 rounded-[24px] min-h-[40px] flex items-center gap-x-0.5 overflow-x-auto'>
-          {sections[0].map((item) => (
-          <ToolbarButton key={item.label} {...item}/>
-      ))}
-    </div>
-  )
+                    onClick: () => editor?.chain().focus().undo().run(),
+                },
+                {
+                    label: "Redo",
+                    icon: Redo2Icon,
+                    onClick: () => editor?.chain().focus().redo().run(),
+                },
+                {
+                    label: "Print",
+                    icon: PrinterIcon,
+                    onClick: () => window.print(),
+                },
+                {
+                    label: "Spell Check",
+                    icon: SpellCheckIcon,
+                    onClick: () => {
+                        const current = editor?.view.dom.getAttribute("spellcheck");
+                        editor?.view.dom.setAttribute("spellcheck", current === "false" ? "true" : "false");
+                    }
+                }
+            ],
+            [
+                {
+                    label: 'Bold',
+                    icon: BoldIcon,
+                    isActive: editor?.isActive("bold"),
+                    onClick: () => editor?.chain().focus().toggleBold().run(),
+                },
+                {
+                    label: 'Italic',
+                    icon: ItalicIcon,
+                    isActive: editor?.isActive("italic"),
+                    onClick: () => editor?.chain().focus().toggleItalic().run(),
+                },
+                {
+                    label: 'Underline',
+                    icon: UnderlineIcon,
+                    isActive: editor?.isActive("underline"),
+                    onClick: () => editor?.chain().focus().setUnderline().run(),
+                },
+            ],
+            [
+                {
+                    label: "Comment",
+                    icon: MessageSquarePlusIcon,
+                    onClick: () => console.log("Comment"),
+                    isActive:false,
+                },
+                {
+                    label: "List todo",
+                    icon: ListTodoIcon,
+                    onClick: () => editor?.chain().focus().toggleTaskList().run(),
+                    isActive: editor?.isActive("taskList"),
+                },
+                {
+                    label: "Remove formatting",
+                    icon: RemoveFormattingIcon,
+                    onClick: () => editor?.chain().focus().unsetAllMarks().run(),
+                },
+            ]
+        ]
+    return (
+        <div className='bg-[#F1F4F9] px-2.5 py-0.5 rounded-[24px] min-h-[40px] flex items-center gap-x-0.5 overflow-x-auto'>
+            {sections[0].map((item) => (
+                <ToolbarButton key={item.label} {...item} />
+            ))}
+            <Separator orientation='vertical' className="h-6 bg-neutral-400" />
+            {sections[1].map((item) => (
+                <ToolbarButton key={item.label} {...item} />
+            ))}
+            <Separator orientation='vertical' className="h-6 bg-neutral-400" />
+            {sections[2].map((item) => (
+                <ToolbarButton key={item.label} {...item} />
+            ))}
+            <Separator orientation='vertical' className="h-6 bg-neutral-400" />
+
+            <Separator orientation='vertical' className="h-6 bg-neutral-400" />
+        </div>
+    )
 }
 
 export default Toolbar
